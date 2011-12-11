e6f51ba0411e60a69bffbd78c4dd8c6a207267ae
jackqin <followfollowme@163.com>
2011-12-11 14:23:06 +0800

    create main stream for 1.1.0-SNAPSHOT

--- target/classes/mysql-config/mysql_alert.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `colorcc`.`user` CHANGE COLUMN `status` `status` ENUM('ONLINE', 'OFFLINE', 'HIDDEN') NOT NULL  ;
-
-
-INSERT INTO USER VALUES(1, 'jack@colorcc.com', 'passwd', 'salt', now(), 'online');
